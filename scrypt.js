18b18f60314343a2cc3a3a4ccad8f5c3a5d23e5f
João Augusto Vieira Ramalho <87734651+Joaoavr@users.noreply.github.com>
2022-06-04 16:46:01 -0300

    Add files via upload

--- scrypt.js
+++ scrypt.js
@@ -1,10 +1,12 @@
-var span = document.getElementById("numero")
+var numeric = document.getElementById("numero")
 var numero = 0;
 
 function  increment() {
     numero = numero + 1;
-    span.innerHTML= numero;}
+    numeric.innerHTML= numero;
+}
 
 function  decrement() {
     numero = numero - 1;
-    span.innerHTML= numero;}
+    numeric.innerHTML= numero;
+}
